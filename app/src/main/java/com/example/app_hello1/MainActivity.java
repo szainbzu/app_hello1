760dabd81b1c78416eb9525e995c90b86d7ab376
szainbzu <szain@birzeit.edu>
2025-03-11 11:10:31 +0200

    version1

---
+++ app/src/main/java/com/example/app_hello1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.app_hello1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnShow;
+    private TextView txtHello;
+    private EditText edtName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnShow = findViewById(R.id.btnShow);
+        txtHello = findViewById(R.id.txtHello);
+        edtName = findViewById(R.id.edtName);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edtName.getText().toString();
+                txtHello.setText("hello, " + name);
+            }
+        });
+    }
+}
